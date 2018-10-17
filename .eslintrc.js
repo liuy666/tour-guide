c95a95afe83a4c27c41749b346593f247d5671da
liuyingjie <liuyj@hqyatu.com>
2018-10-17 17:35:31 +0800

    xxxx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
 	root: true,
 	env: {
 		node: true
@@ -20,24 +19,3 @@ module.exports = {
 		AMap: true
 	}
 }
-=======
-    root: true,
-    env: {
-        node: true
-    },
-    'extends': [
-        'plugin:vue/essential',
-        'eslint:recommended'
-    ],
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-    },
-    parserOptions: {
-        parser: 'babel-eslint'
-    },
-    globals: {
-        AMap: true
-    }
-}
->>>>>>> 59177fe0a6358e5b9dac48e2d69b330cc7a694ba
