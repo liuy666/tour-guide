40c0c7085291f0a4785fe51acf9a4dadffe01809
yangmei <1036720734@qq.com>
2018-11-28 16:16:30 +0800

    xxx

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -770,7 +770,11 @@
             //     minZoom: 17
             // }).addTo(oMap);
             
-            L.imageOverlay(scenicBgImg, [imgLeftBottom1,imgRightTop1]).addTo(oMap);
+            let mapImg = L.imageOverlay(scenicBgImg, [imgLeftBottom1,imgRightTop1]).addTo(oMap);
+            mapImg.on('load',function(){
+                
+            })
+
             this.oMap_main = oMap;
 
             oMap.on('click',function(e) {
@@ -1671,7 +1675,8 @@
                     let playStatus = {
                         currentTime: document.querySelector('.main-audio').currentTime,
                         totalTime: this.totalTime,
-                        isPauseStatus : status
+                        isPauseStatus : status,
+                        resourceId: document.querySelector('.main-audio').dataset.id
                     }
                     sessionStorage.setItem('playStatus', JSON.stringify(playStatus));
                 }
